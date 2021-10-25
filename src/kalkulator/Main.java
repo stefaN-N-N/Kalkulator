e335dba0bf332a279f94e62910930c805fc96eb6
Stefan Lepsanovic <stefanlepsanovic@gmail.com>
2021-10-25 16:50:31 +0200

    initial read-loop

--- src/kalkulator/Main.java
+++ src/kalkulator/Main.java
@@ -1,9 +1,18 @@
 package kalkulator;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		while (true) {
+			Scanner s1 = new Scanner(System.in);
+			String a;
+			a = s1.nextLine();
+			if(a.equals("exit") || a.equals("kraj")) {
+				break;
+			}
+		}
 
 	}
 
